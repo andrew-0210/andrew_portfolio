6b90d42d58921ae1961f285c4ed9b35fd63d0472
Andrew Monthero <166970322+andrew-0210@users.noreply.github.com>
2025-01-08 20:08:54 +1100

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
   const [isVisible,setIsVisible] = useState(false);
 
   const handleIsVisible = () => {
-    setIsVisible(!visible);
+    setIsVisible(!isVisible);
   }
    
   return (
@@ -28,26 +28,23 @@ const Hero = () => {
             function flawlessly.
           </p>
           <div>
-            <div className='rounded-full bg-[#33793B] py-[0.5rem] px-[1.5rem] text-[#fff] text-center inline-flex items-center justify-center gap-2 '>
+            <div className='rounded-full bg-[#33793B] py-[0.5rem] px-[1.5rem] text-[#fff] text-center inline-flex items-center justify-center gap-2 ' onClick={handleIsVisible}>
               <div className='relative'>
                 <div className='w-3 h-3 bg-[#6FE485] rounded-full animate-ping absolute' />
                 <div className='w-3 h-3 bg-[#6FE485] rounded-full' />
               </div>
-              <div>
-              <div
-        className='px-[2rem] py-[0.5rem] bg-[#3a3a3a] inline-block rounded-full'
-        onClick={handleVisible}
-      >
+        
+    
         Working at <span className='font-bold '>Adaptify Digital</span>
-      </div>
-      {visible && (
+ 
+      {isVisible && (
         <div className='absolute top-1/2 right-0 transform translate-x-full -translate-y-1/2 px-4 py-[0.75rem] bg-[#dfbf76] rounded-full'>
           <p className='inline-block text-[#1a1a1a] font-semibold'>
             Since March 2024
           </p>
         </div>
       )}
-              </div>
+    
               
             </div>
           </div>
