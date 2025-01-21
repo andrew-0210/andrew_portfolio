effe4e468099b8d18480c56f10052c1f468fdee1
Andrew Monthero <166970322+andrew-0210@users.noreply.github.com>
2025-01-21 19:33:12 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,7 +31,9 @@ const Projects = () => {
             href={proj.href}
             key={index}
             className='lg:min-w-[573px] w-[23rem] h-full gap-2 flex flex-col'
-          >
+            passHref
+            legacyBehaviour 
+          ><a target="_blank" rel="noopener noreferrer">
             <div className='relative w-full h-[18rem] lg:h-[25.5rem] lg:w-[35.813rem]'>
               <Image
                 src={proj.pic}
@@ -47,6 +49,7 @@ const Projects = () => {
                 <h5 className='text-[#B7AB98]'>{proj.type}</h5>
               </span>
             </div>
+           </a>
           </Link>
         );
       })}
