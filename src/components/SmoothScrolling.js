c0b697e5e989ec14b8534becf9c5a1242712ed49
Andrew Monthero <166970322+andrew-0210@users.noreply.github.com>
2025-02-07 16:42:33 +1100

    Create SmoothScrolling.js

---
+++ src/components/SmoothScrolling.js
@@ -0,0 +1,15 @@
+'use client';
+
+import { ReactLenis } from 'lenis/react';
+
+export default function SmoothScrolling({ children }) {
+  // const lenis = useLenis(({ scroll }) => {
+  //   // called every scroll
+  // });
+
+  return (
+    <ReactLenis root options={{ duration: 5, lerp: 0.2, syncTouch: true }}>
+      {children}
+    </ReactLenis>
+  );
+}
